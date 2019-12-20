1a8fd5a54562c29eec4636f855a13cee6a59b4d4
Victor Coatalem <victor.coatalem@epita.fr>
2019-12-20 16:28:17 +0100

    [TEST] updated test.sh file for remote test automation

--- kmeans/test.sh
+++ kmeans/test.sh
@@ -1,2 +1,10 @@
+CURRENT=$PWD
+DIR=$(dirname $0)
+cd $DIR
+
 make
-./kmeans 3 20 1.0 2351 200000 ../ember/Xtest.dat out.dat
+./kmeans 3 20 1.0 2351 900000 ../ember/Xtrain.dat out.dat
+
+python3 ../python/accuracy/classification_eval.py out.dat ../ember/Ytrain.dat
+
+cd $CURRENT
