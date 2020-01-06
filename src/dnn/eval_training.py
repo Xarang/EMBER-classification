b736eb4fd5c3119475ffdc04bc972bdc70207e28
Victor Coatalem <victor.coatalem@epita.fr>
2020-01-06 12:25:33 +0100

    [MISC] * updated setup.sh to set some environment variables in python virtual env * renamed extract validation set to extract dataset (more fitting) * removed old model directory, placed it in dnn directory * fixed some issues with eval_evaluation.py

--- src/dnn/eval_training.py
+++ src/dnn/eval_training.py
@@ -1,4 +1,15 @@
 import dnn
 import sys
+import os
 
-dnn.train_and_save(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+xtrainfile=os.getenv('TRAINING_DATA')
+xvalidationfile=os.getenv('VALIDATION_DATA')
+ytrainfile=os.getenv('TRAINING_LABELS')
+yvalidationfile=os.getenv('VALIDATION_LABELS')
+
+if xtrainfile == None or xvalidationfile == None or ytrainfile == None or yvalidationfile == None:
+    print("[EVAL] error: one of 'TRAINING_DATA', 'VALIDATION_DATA', 'TRAINING_LABELS', 'VALIDATION_LABELS' env variable is not set.")
+    print("[EVAL] make sure you run the ./setup script at the root of the project or set these variables manually")
+    exit
+
+dnn.train_and_save(xtrainfile, ytrainfile, xvalidationfile, yvalidationfile)
